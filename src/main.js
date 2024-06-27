93bf1dfd00ffe2280fb08bd95d9513204eb584fa
Paulo Duarte <p.roger.antunes43@gmail.com>
2024-06-27 00:10:01 -0300

    Fix GTM

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import { useGtm } from 'vue-gtm'
+import VueGtm from 'vue-gtm';
 
-createApp(App).use(useGtm, { id: 'GTM-P6JXHL5K' }).mount('#app')
+createApp(App).use(VueGtm, { id: 'GTM-P6JXHL5K' }).mount('#app')
